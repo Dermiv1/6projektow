a407391e009af1f4c349563bd33cb33e5be6f819
Dermiv1 <spiderlazalus@gmail.com>
2024-10-30 16:12:24 +0100

    praca 2/6

---
+++ AsyncOperationCalculator.mjs
@@ -0,0 +1,21 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function getNumberInput(prompt) {
+  return new Promise((resolve) => {
+    rl.question(prompt, (input) => {
+      const number = parseFloat(input);
+      if (isNaN(number)) {
+        console.error("Błąd: Podana wartość nie jest liczbą.");
+        rl.close();
+        process.exit(1);
+      } else {
+        resolve(number);
+      }
+    });
+  });
+}
